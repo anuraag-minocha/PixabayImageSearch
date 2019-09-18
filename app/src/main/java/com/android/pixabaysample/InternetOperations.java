bb81491f5e8194677fb9a754d32ba18de0e5b0c3
Anuraag Minocha <anuraag.minocha@gmail.com>
2019-09-18 16:47:02 +0530

    Created main activity layout

---
+++ app/src/main/java/com/android/pixabaysample/InternetOperations.java
@@ -0,0 +1,31 @@
+package com.android.pixabaysample;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class InternetOperations {
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static String SERVER_URL = "https://pixabay.com/api/";
+    public static String API_KEY = "13665803-562bbd6db6230116df784dc4b";
+
+    public static String get(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+        Request request = new Request.Builder().url(url).post(body).build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
--- app/src/main/java/com/android/pixabaysample/MainActivity.java
+++ app/src/main/java/com/android/pixabaysample/MainActivity.java
@@ -2,12 +2,30 @@ package com.android.pixabaysample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etSearch;
+    ImageView ivSearch;
+    ProgressBar topProgressBar;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeView();
+
+    }
+
+    public void initializeView(){
+        etSearch = findViewById(R.id.etSearchbar);
+        ivSearch = findViewById(R.id.ivSearch);
+        topProgressBar = findViewById(R.id.topProgressBar);
+        recyclerView = findViewById(R.id.recyclerView);
     }
 }
